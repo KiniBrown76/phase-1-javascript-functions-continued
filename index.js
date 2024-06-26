b8c21eb5d239a10bed3ffe7eabab0e9fbb64e9bf
KiniBrown76 <esther.kini@student.moringaschool.com>
2024-06-26 10:50:31 +0300

    add js function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// function declaration
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// function mondayWork
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+// ffunction wrapAdjective
+function wrapAdjective(wraps = '*'){
+    return function(adjective = '||') {
+        return `You are ${wraps}${adjective}${wraps}!`;
+    }
+}
